9a03d6249cb9bac45d70f68e26fdbbb04daa3318
adorahelen <adorahelenmin@gmail.com>
2024-09-10 23:14:14 +0900

    Initial commit

---
+++ src/main/java/kdt/hackathon/grandchildren/GrandchildrenApplication.java
@@ -0,0 +1,13 @@
+package kdt.hackathon.grandchildren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandchildrenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandchildrenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdt/hackathon/grandchildren/GrandchildrenApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.hackathon.grandchildren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandchildrenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
